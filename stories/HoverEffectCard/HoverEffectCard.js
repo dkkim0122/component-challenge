64c80fee6e35330edd0bef4a918a01f47b616b57
kim-dokyung <dokyung.kim@flitto.com>
2022-12-22 12:27:11 +0900

    05. Hover Effect Card: 애니메이션 적용

--- stories/HoverEffectCard/HoverEffectCard.js
+++ stories/HoverEffectCard/HoverEffectCard.js
@@ -4,30 +4,38 @@ export const createHoverEffectCard = () => {
   const container = document.createElement('article')
   container.classList.add('hover-effect-card__container')
 
-  const card = document.createElement('div')
-  card.classList.add('hover-effect-card__card')
+  const createCard = (sequence) => {
+    const card = document.createElement('div')
+    card.classList.add('hover-effect-card__card')
 
-  const cardHeader = document.createElement('h2')
-  cardHeader.innerText = 'First Card'
-  cardHeader.classList.add('hover-effect-card__header')
-  
-  const cardContent = document.createElement('div')
-  cardContent.classList.add('hover-effect-card__content')
-  
-  const cardText = document.createElement('p')
-  cardText.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio nisi reprehenderit error culpa amet incidunt architecto numquam ad at dolorem odit maxime vel dolor mollitia sapiente, aperiam, quo quae?'
-  cardText.classList.add('hover-effect-card__text')
+    const cardHeader = document.createElement('h2')
+    cardHeader.innerText = `${sequence} Card`
+    cardHeader.classList.add('hover-effect-card__header')
+    
+    const cardContent = document.createElement('div')
+    cardContent.classList.add('hover-effect-card__content')
+    
+    const cardText = document.createElement('p')
+    cardText.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius optio nisi reprehenderit error culpa amet incidunt architecto numquam ad at dolorem odit maxime vel dolor mollitia sapiente, aperiam, quo quae?'
+    cardText.classList.add('hover-effect-card__text')
 
-  const cardButton = document.createElement('a')
-  cardButton.innerText = 'Read More'
-  cardButton.setAttribute('href', '#')
-  cardButton.classList.add('hover-effect-card__button')
-  
-  cardContent.appendChild(cardHeader)
-  cardContent.appendChild(cardText)
-  cardContent.appendChild(cardButton)
-  card.appendChild(cardContent)
-  container.appendChild(card)
+    const cardButton = document.createElement('a')
+    cardButton.innerText = 'Read More'
+    cardButton.setAttribute('href', '#')
+    cardButton.classList.add('hover-effect-card__button')
+    
+    cardContent.appendChild(cardHeader)
+    cardContent.appendChild(cardText)
+    cardContent.appendChild(cardButton)
+    card.appendChild(cardContent)
+    container.appendChild(card)
+  }
+
+  const sequenceOfCard = ['First', 'Second', 'Third']
+
+  sequenceOfCard.forEach(sequence => {
+    createCard(sequence)
+  })
 
   return container
 }
