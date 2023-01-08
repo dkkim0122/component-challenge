7db61271fd6d0de1e4525d4d8ac02a994f31a3af
Dokyung Kim <dkkim0122@gmail.com>
2023-01-08 23:30:43 +0900

    07.Fake Scroll Spy: CSS 작업중

--- stories/FakeScrollSpy/FakeScrollSpy.js
+++ stories/FakeScrollSpy/FakeScrollSpy.js
@@ -1,26 +1,28 @@
+import './FakeScrollSpy.css'
+
 export const createFakeScrollSpy = () => {
   const container = document.createElement('article')
+  container.id = 'article'
 
   const nav = document.createElement('nav')
+  nav.classList.add('nav')
   const navBar = `
-    <ul>
-      <li class="fake-scroll-spy__nav-menu"><a href="#home" class="fake-scroll-spy__nav-anchor"><span>Home</span></a></li>
-      <li class="fake-scroll-spy__nav-menu"><a href="#about" class="fake-scroll-spy__nav-anchor"><span>About</span></a></li>
-      <li class="fake-scroll-spy__nav-menu"><a href="#services" class="fake-scroll-spy__nav-anchor"><span>Services</span></a></li>
-      <li class="fake-scroll-spy__nav-menu"><a href="#portfolio" class="fake-scroll-spy__nav-anchor"><span>Portfolio</span></a></li>
-      <li class="fake-scroll-spy__nav-menu"><a href="#contact" class="fake-scroll-spy__nav-anchor"><span>Contact</span></a></li>
-    </ul>
+      <a href="#home" class="nav-menu"><span>Home</span></a>
+      <a href="#about" class="nav-menu"><span>About</span></a>
+      <a href="#services" class="nav-menu"><span>Services</span></a>
+      <a href="#portfolio" class="nav-menu"><span>Portfolio</span></a>
+      <a href="#contact" class="nav-menu"><span>Contact</span></a>
   `
   nav.innerHTML = navBar
 
   const main = document.createElement('main')
   const mainSections = `
   <ul>
-    <li class="fake-scroll-spy__section"><span>Home Section</span></li>
-    <li class="fake-scroll-spy__section"><span>About Section</span></li>
-    <li class="fake-scroll-spy__section"><span>Services Section</span></li>
-    <li class="fake-scroll-spy__section"><span>Portfolio Section</span></li>
-    <li class="fake-scroll-spy__section"><span>Contact Section</span></li>
+    <li class="section"><span>Home Section</span></li>
+    <li class="section"><span>About Section</span></li>
+    <li class="section"><span>Services Section</span></li>
+    <li class="section"><span>Portfolio Section</span></li>
+    <li class="section"><span>Contact Section</span></li>
   </ul>
   `
   main.innerHTML = mainSections
